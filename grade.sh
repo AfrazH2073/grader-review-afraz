2d4a98b64001f07b1f5c6f28895780052098c610
Afraz Hameed <ahameed@MacBook-Pro-4.local>
2023-11-27 14:40:45 -0800

    new updates

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,25 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+
+cp -r student-submission/*.java ./grading-area
+cp TestListExamples.java ./grading-area
+
+cd grading-area
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > text.txt
+
+files=`find student-submission/*.java`
+for file in $files
+do
+    if [[ -f $file ]]
+    then 
+       java $file > text.txt
+        grep -o "Failures: " text.txt
+        echo "this works"
+    else
+        echo "incorrect path"
+    fi
+
+done
